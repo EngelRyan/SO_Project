b044799be14d6ee86cb363b08f6a4673b9ef1652
EngelRyan <ryaneengel@gmail.com>
2024-04-08 19:46:59 -0300

    Update SJF & Priority

--- Main.java
+++ Main.java
@@ -47,10 +47,14 @@ public class Main {
                     SJF(false);
                     break;
 
-                case "4":
+                case "4"://PRIORIDADE PREEMPTIVO
+                    Priority(true);
                     break;
-                case "5":
+
+                case "5"://PRIORIDADE NÃO PREEMPTIVO
+                    Priority(false);
                     break;
+
                 case "6":
                     break;
 
@@ -195,6 +199,80 @@ public class Main {
                     if (running_tasks.get(small_bt).getTl() == 0) {
                         new_arrival = true;
                         running_tasks.remove(running_tasks.get(small_bt));
+                    }
+                }
+            } else {//caso ainda não tenham chegado algum processo
+                System.out.printf("Time [%d]: No task ready\n", time);
+            }
+            time++;
+            has_task = false;
+            //verifica se todas as tasks foram concluidas, se sim encerra o loop
+            for (Task task : tasks) {
+                if (task.getTl() > 0) {
+                    has_task = true;
+                    break;
+                }
+            }
+
+        } while (has_task);
+        print_stats();
+        reset_stats();
+    }
+    public static void Priority(boolean preempt){
+        //implementar código Prioridade
+        int time = 1;
+        boolean new_arrival = true;
+        int last_task = -1;
+        boolean has_task;
+
+        ArrayList<Task> running_tasks = new ArrayList<>();
+
+        //Loop que roda enquanto existem tarefas com tempo restante diferente de 0
+        do {//Verifica se algum processo ja chegou e se já não foi add em running_tasks
+            // se atender as condições add a tarefa em running_tasks
+            for (Task task : tasks) {
+                if (task.getAt() <= time && !running_tasks.contains(task) && task.getTl() != 0) {
+                    running_tasks.add(task);
+                }
+            }
+            //váriavel para pegar a tarefa com a maior prioridade
+            int high_prio = 0;
+            if (preempt) {//se for preemptivo pega a terefa com maior prioridade
+                for (int i = 0; i < running_tasks.size(); i++) {
+                    if (running_tasks.get(i).getPrio() > running_tasks.get(high_prio).getPrio()) {
+                        high_prio = i;
+                    }
+                }
+            } else {// se for não preemptivo organiza em ordem de chegada
+                running_tasks.sort(Comparator.comparingInt(Task::getAt));
+
+                for (int i = 0; i < running_tasks.size(); i++) {
+                    for (int j = 1; j < running_tasks.size(); j++) {
+                        Task task1 = running_tasks.get(i);
+                        Task task2 = running_tasks.get(j);
+                        if (task1.getAt() == task2.getAt() && task1.getPrio() < task2.getPrio()) {
+                            // Trocar tarefas se o tempo de chegada for igual e a prioridade de task2 for menor
+                            running_tasks.set(i, task2);
+                            running_tasks.set(j, task1);
+                        }
+                    }
+                }
+            }
+            if (!running_tasks.isEmpty()) {// se a lista não estiver vazia entra
+                //verifica se chegou um novo processo, se não chegou seta o tempo de espera
+                if (new_arrival || last_task != running_tasks.get(high_prio).getNumber()) {
+                    running_tasks.get(high_prio).setWt(time - running_tasks.get(high_prio).getAt()-(running_tasks.get(high_prio).getBt() - running_tasks.get(high_prio).getTl()));
+                    new_arrival = false;
+                }
+                //se o tempo restante for diferente de 0 diminui 1 do tempo e imprime, depois atribui o número da task a last_task
+                if (running_tasks.get(high_prio).getTl() != 0) {
+                    running_tasks.get(high_prio).setTl(running_tasks.get(high_prio).getTl() - 1);
+                    System.out.printf("Time[%d]: Task[%d] Timeleft= %d\n", time, running_tasks.get(high_prio).getNumber(), running_tasks.get(high_prio).getTl());
+                    last_task = running_tasks.get(high_prio).getNumber();
+                    //se for == 0 remove da lista
+                    if (running_tasks.get(high_prio).getTl() == 0) {
+                        new_arrival = true;
+                        running_tasks.remove(running_tasks.get(high_prio));
                     }
                 }
             } else {//caso ainda não tenham chegado algum processo
